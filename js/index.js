a4b45ac6cd90c5af8de7d8ffa737714dad11c0f0
ZackNemec <zacknemec@live.com>
2020-07-10 20:29:43 -0700

    1st event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const nav = document.querySelector(".nav");
+nav.addEventListener("mouseenter", () => {
+  nav.style.transform = "scale(1.2)";
+  nav.style.font.weight = "900";
+});
